ac017cfd5c96971a71ad055a965e6b94a7ddd584
ggdodo <satriyo04@gmail.com>
2020-04-20 21:32:50 +0700

    perbaikan database

--- db_kelulusan.sql
+++ db_kelulusan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2020 at 03:25 PM
+-- Generation Time: Apr 20, 2020 at 04:30 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -41,7 +41,8 @@ CREATE TABLE `tbl_hubungi` (
 
 INSERT INTO `tbl_hubungi` (`id_hubungi`, `nama`, `email`, `pesan`) VALUES
 (3, 'adi kiswanto', 'adikiswanto@gmail.com', 'halo halo'),
-(15, 'paijo', 'mtsaddarain@gmail.com', 'halo halo bandung');
+(15, 'paijo', 'mtsaddarain@gmail.com', 'halo halo bandung'),
+(16, 'Adi Supardi', 'agusrahman@gmail.com', 'jhjhgjgkhkjhk');
 
 -- --------------------------------------------------------
 
@@ -108,8 +109,9 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id_user`, `username`, `pass`, `nama`, `level_user`, `log_activity`) VALUES
-(4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Ahmad Kusnadi', 0, '18-04-2020 13+7:36:45'),
-(10, 'adiman', 'd41d8cd98f00b204e9800998ecf8427e', 'Adi Manahan', 0, '0000-00-00');
+(4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Ahmad Kusnadi', 0, '20-04-2020 16+7:21:23'),
+(10, 'adiman', 'd41d8cd98f00b204e9800998ecf8427e', 'Adi Manahan', 1, '0000-00-00'),
+(11, 'saipul', '0df98fedfb246b51562ba130fc39b376', 'Saipulloh', 1, '20-04-2020 16+7:03:37');
 
 -- --------------------------------------------------------
 
@@ -118,24 +120,24 @@ INSERT INTO `tbl_user` (`id_user`, `username`, `pass`, `nama`, `level_user`, `lo
 --
 
 CREATE TABLE `tb_student` (
-  `no` varchar(200) NOT NULL,
+  `no` int(5) NOT NULL,
   `noujian` varchar(200) NOT NULL,
   `nama` varchar(200) NOT NULL,
   `tgllhr` varchar(200) NOT NULL,
-  `addresse` varchar(200) NOT NULL,
-  `sekolah` varchar(200) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `jurusan` varchar(200) NOT NULL,
   `indo` varchar(200) NOT NULL,
   `mat` varchar(200) NOT NULL,
   `inggris` varchar(200) NOT NULL,
-  `ipa` varchar(200) NOT NULL,
+  `kejuruan` varchar(200) NOT NULL,
   `nsindo` varchar(200) NOT NULL,
   `nsinggris` varchar(200) NOT NULL,
   `nsmat` varchar(200) NOT NULL,
-  `nsipa` varchar(200) NOT NULL,
+  `nskejuruan` varchar(200) NOT NULL,
   `naindo` varchar(200) NOT NULL,
   `nainggris` varchar(200) NOT NULL,
   `namat` varchar(200) NOT NULL,
-  `naipa` varchar(200) NOT NULL,
+  `nakejuruan` varchar(200) NOT NULL,
   `rataun` varchar(200) NOT NULL,
   `ratans` varchar(200) NOT NULL,
   `ratana` varchar(200) NOT NULL,
@@ -147,12 +149,17 @@ CREATE TABLE `tb_student` (
 -- Dumping data for table `tb_student`
 --
 
-INSERT INTO `tb_student` (`no`, `noujian`, `nama`, `tgllhr`, `addresse`, `sekolah`, `indo`, `mat`, `inggris`, `ipa`, `nsindo`, `nsinggris`, `nsmat`, `nsipa`, `naindo`, `nainggris`, `namat`, `naipa`, `rataun`, `ratans`, `ratana`, `ket`, `ketgambar`) VALUES
-('1', '27-807-001-8', 'Adam Mulyana', '16 Oktober 1998', 'Kediri', '2', '70', '80', '78', '79', '80', '81', '67', '66', '55', '67', '55', '75', '76.75', '73.5', '63', 1, 'lulus'),
-('2', '27-807-002-7', 'Adi Prasetyo', '17 Oktober 1998', 'Kediri', '3', '80', '78', '80', '76', '70', '89', '78', '77', '66', '88', '66', '76', '78.5', '78.5', '74', 2, 'ditangguhkan'),
-('3', '27-807-003-6', 'Ahmad Syahrijal', '18 Oktober 2000', 'Kediri', '5', '67', '56', '56', '56', '70', '57', '79', '88', '55', '77', '77', '65', '58.75', '73.5', '68.5', 1, 'lulus'),
-('4', '27-807-004-5', 'Sakir Muhammad', '19 Oktober 1998', 'Kediri', '3', '56', '45', '45', '45', '80', '76', '67', '77', '77', '66', '66', '55', '47.75', '75', '66', 0, 'tidaklulus'),
-('5', '27-807-005-4', 'Afi Ariyati', '20 Oktober 1998', 'Kediri', '3', '56', '65', '65', '65', '70', '75', '70', '66', '88', '77', '55', '65', '62.75', '70.25', '71.25', 0, 'tidaklulus');
+INSERT INTO `tb_student` (`no`, `noujian`, `nama`, `tgllhr`, `alamat`, `jurusan`, `indo`, `mat`, `inggris`, `kejuruan`, `nsindo`, `nsinggris`, `nsmat`, `nskejuruan`, `naindo`, `nainggris`, `namat`, `nakejuruan`, `rataun`, `ratans`, `ratana`, `ket`, `ketgambar`) VALUES
+(13, '27-807-001-8', 'Ahmad Sobri', '20-02-2000', 'kediri', '1', '70', '80', '78', '79', '80', '81', '67', '66', '55', '67', '55', '75', '76.75', '73.5', '63', 0, 'lulus'),
+(14, '27-807-002-7', 'Adi Hidayat', '15-02-2001', 'kediri', '2', '80', '78', '80', '76', '70', '89', '78', '77', '66', '88', '66', '76', '78.5', '78.5', '74', 0, 'lulus'),
+(15, '27-807-003-6', 'Ahmed Saiful', '16-02-2000', 'kediri', '2', '67', '56', '56', '56', '70', '57', '79', '88', '55', '77', '77', '65', '58.75', '73.5', '68.5', 0, 'lulus'),
+(16, '27-807-004-5', 'Sunnan', '15-03-2000', 'kediri', '1', '56', '45', '45', '45', '80', '76', '67', '77', '77', '66', '66', '55', '47.75', '75', '66', 0, 'tidaklulus'),
+(17, '27-807-005-4', 'Afiyati', '18-05-2001', 'kediri', '1', '56', '65', '65', '65', '70', '75', '70', '66', '88', '77', '55', '65', '62.75', '70.25', '71.25', 0, 'tidaklulus'),
+(18, '27-807-001-8', 'Ahmad Sobri', '20-02-2000', 'kediri', '1', '70', '80', '78', '79', '80', '81', '67', '66', '55', '67', '55', '75', '76.75', '73.5', '63', 0, 'lulus'),
+(19, '27-807-002-7', 'Adi Hidayat', '15-02-2001', 'kediri', '2', '80', '78', '80', '76', '70', '89', '78', '77', '66', '88', '66', '76', '78.5', '78.5', '74', 0, 'lulus'),
+(20, '27-807-003-6', 'Ahmed Saiful', '16-02-2000', 'kediri', '2', '67', '56', '56', '56', '70', '57', '79', '88', '55', '77', '77', '65', '58.75', '73.5', '68.5', 0, 'lulus'),
+(21, '27-807-004-5', 'Sunnan', '15-03-2000', 'kediri', '1', '56', '45', '45', '45', '80', '76', '67', '77', '77', '66', '66', '55', '47.75', '75', '66', 0, 'tidaklulus'),
+(22, '27-807-005-4', 'Afiyati', '18-05-2001', 'kediri', '1', '56', '65', '65', '65', '70', '75', '70', '66', '88', '77', '55', '65', '62.75', '70.25', '71.25', 0, 'tidaklulus');
 
 --
 -- Indexes for dumped tables
@@ -197,7 +204,7 @@ ALTER TABLE `tb_student`
 -- AUTO_INCREMENT for table `tbl_hubungi`
 --
 ALTER TABLE `tbl_hubungi`
-  MODIFY `id_hubungi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_hubungi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tbl_jurusan`
@@ -215,7 +222,13 @@ ALTER TABLE `tbl_kode`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tb_student`
+--
+ALTER TABLE `tb_student`
+  MODIFY `no` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
